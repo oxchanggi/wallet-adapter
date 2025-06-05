19e2dfbc8e5d94e3fb4767cae01125d12bbd396d
John <johnwick.bk99@gmail.com>
2025-06-05 17:39:32 +0700

    change prettier

--- src/phoenix-wallet/connectors/evm/BinanceEvmConnector.ts
+++ src/phoenix-wallet/connectors/evm/BinanceEvmConnector.ts
@@ -1,11 +1,11 @@
-import { getProvider } from '@binance/w3w-ethereum-provider'
-import { DappMetadata } from '../types'
-import { EvmConnector } from './EvmConnector'
+import { getProvider } from '@binance/w3w-ethereum-provider';
+import { DappMetadata } from '../types';
+import { EvmConnector } from './EvmConnector';
 
 // const binanceProvider = getProvider()
 
 export class BinanceEvmConnector extends EvmConnector {
-  private binanceProvider: any
+  private binanceProvider: any;
   constructor(dappMetadata: DappMetadata) {
     super(
       'binanceevm',
@@ -14,40 +14,40 @@ export class BinanceEvmConnector extends EvmConnector {
         logo: 'https://static.bnbchain.org/home-ui/static/images/wallets/logo_binance-web3-wallet.svg?v=1',
       },
       dappMetadata
-    )
+    );
   }
   get provider(): any {
-    return this.binanceProvider
+    return this.binanceProvider;
   }
 
   async init(): Promise<void> {
     if (this.isInitialized) {
-      return
+      return;
     }
 
-    this.isInitialized = true
-    this.binanceProvider = getProvider()
+    this.isInitialized = true;
+    this.binanceProvider = getProvider();
 
     if (!this.provider) {
-      throw new Error(this.name + ' provider not found')
+      throw new Error(this.name + ' provider not found');
     }
 
-    this.setupEventListeners()
+    this.setupEventListeners();
 
     // Check if we have a stored connection
-    this.checkStoredConnection()
+    this.checkStoredConnection();
   }
 
   async disconnect(): Promise<void> {
-    await this.provider.disconnect()
-    await super.disconnect()
+    await this.provider.disconnect();
+    await super.disconnect();
   }
 
   get storageConnectionStatusKey(): string | null {
-    return null
+    return null;
   }
 
   async isInstalled(): Promise<boolean> {
-    return true
+    return true;
   }
 }
--- src/phoenix-wallet/index.ts
+++ src/phoenix-wallet/index.ts
@@ -1,28 +1,28 @@
 // Re-export types
-export * from './chains/Chain'
-export * from './chains/EvmChain'
-export * from './chains/SolanaChain'
-export * from './wallets/IWallet'
+export * from './chains/Chain';
+export * from './chains/EvmChain';
+export * from './chains/SolanaChain';
+export * from './wallets/IWallet';
 
-export * from './connectors/evm/BinanceEvmConnector'
-export * from './connectors/evm/EvmConnector'
-export * from './connectors/evm/MagicEdenEvmConnector'
-export * from './connectors/evm/MetamaskEvmConnector'
-export * from './connectors/evm/PhantomEvmConnector'
-export * from './connectors/evm/RainbowEvmConnector'
-export * from './connectors/evm/TrustWalletEvmConnector'
-export * from './connectors/IConnector'
-export * from './connectors/index'
-export * from './connectors/solana/SolanaConnector'
-export * from './connectors/types'
+export * from './connectors/evm/BinanceEvmConnector';
+export * from './connectors/evm/EvmConnector';
+export * from './connectors/evm/MagicEdenEvmConnector';
+export * from './connectors/evm/MetamaskEvmConnector';
+export * from './connectors/evm/PhantomEvmConnector';
+export * from './connectors/evm/RainbowEvmConnector';
+export * from './connectors/evm/TrustWalletEvmConnector';
+export * from './connectors/IConnector';
+export * from './connectors/index';
+export * from './connectors/solana/SolanaConnector';
+export * from './connectors/types';
 
 // Contexts
-export { useWalletConnectors, WalletProvider } from './contexts/WalletContext'
+export { useWalletConnectors, WalletProvider } from './contexts/WalletContext';
 
 // Hooks
-export { useWallet } from './hooks/useWallet'
-export { useWalletConnectorEvent } from './hooks/useWalletConnectorEvent'
+export { useWallet } from './hooks/useWallet';
+export { useWalletConnectorEvent } from './hooks/useWalletConnectorEvent';
 
 // Types
-export { ConnectorStatus } from './connectors/types'
-export type { WalletContextState } from './connectors/types'
+export { ConnectorStatus } from './connectors/types';
+export type { WalletContextState } from './connectors/types';
